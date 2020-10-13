0e204e87286f4864664aa58bc07ca1445653453c
d-mcgrath <mcgrath1@mit.edu>
2020-10-13 14:53:46 -0400

    improved tool speed, updated read_data
    Former-commit-id: dcd7a2254f0eba16e221a573f7d52b5ecfc6aca6

--- MetaPredict/R/read_data.R
+++ MetaPredict/R/read_data.R
@@ -2,6 +2,7 @@
 #' Each file should end in "-ko.tsv" and contain a column of KEGG Orthology terms with column title 'KO'
 #' and a column with their associated HMM or Blast E-values titled 'E-value'.
 #' @param Data A dataframe or list of dataframes of HMM/Blast results which include KEGG Orthology terms and E-values in two of the columns of each dataframe
+#' @param File The full or relative path to a single input flatfile
 #' @param filePath The full path to the input file or files
 #' @param filePattern The file extension pattern. If using multiple files, this will read in all files in the listed directory
 #' @param kofamscan If the input file or files are output from Kofamscan, set this argument to TRUE, otherwise FALSE. Default is TRUE
@@ -9,15 +10,18 @@
 #' @param delim The delimiter of the input files. Default is tab
 #' @importFrom magrittr "%>%"
 #' @export
-read_data <- function(Data = NULL, filePath = NULL, filePattern = '*-ko.tsv', #still needs to accept single input files more easily...
+read_data <- function(Data = NULL, File = NULL, filePath = NULL, filePattern = '-ko.tsv', #still needs to accept single input files more easily...
                       kofamscan = TRUE, evalue = 1e-3, delim = '\t') {
 
   message('Parsing HMM/Blast hits and E-values into MetaPredict. Using E-value cutoff: ', evalue)
   res <- list()
 
-  if (is.null(Data)) {
+  if (is.null(Data) & is.null(File)) {
     files <- list.files(path = filePath, pattern = filePattern, full.names = TRUE)
     index <- length(files)
+  }
+  if (is.null(Data) & !is.null(File)) {
+    index <- length(File)
   }
   if (is.list(Data) & purrr::every(Data, ~ {
     grepl(paste(names(.x), collapse = '|'),
@@ -27,9 +31,13 @@ read_data <- function(Data = NULL, filePath = NULL, filePattern = '*-ko.tsv', #s
     orgName <- substitute(Data)
   }
   res <- furrr::future_map(1:index, .progress = T, ~ {
-    if (is.null(Data)) {
-      col <- sub(paste('(.*)', filePattern, sep = ''), '\\1', index[.x], perl = T)
-      res[[.x]] <- readr::read_delim(index[.x], col_types = readr::cols(), delim = delim)
+    if (is.null(Data) & is.null(File)) {
+      col <- sub(paste('.*\\/(.*)', filePattern, sep = ''), '\\1', files[.x], perl = T)
+      res[[.x]] <- readr::read_delim(files[.x], col_types = readr::cols(), delim = delim)
+
+    } else if (is.null(Data) & !is.null(File)) {
+      col <- sub('.*\\/(.*)\\..*', '\\1', File, perl = T)
+      res[[.x]] <- readr::read_delim(File, col_types = readr::cols(), delim = delim)
 
     } else if (is.data.frame(Data) & length(list(Data)) == 1) {
       res[[.x]] <- Data
