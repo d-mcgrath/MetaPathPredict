2e278a9d629327055c45aed4d27e2967ff741c2c
d-mcgrath <mcgrath1@mit.edu>
2020-11-04 15:28:16 -0500

    updated internal data, defensive programming, heatmap output format, cli interfacing

--- MetaPredict/R/MetaPredict.R
+++ MetaPredict/R/MetaPredict.R
@@ -8,6 +8,7 @@
 #' @param output_dir The full or relative path to an output directory where result and summary output will be saved as TSV flatfiles
 #' @importFrom magrittr "%>%"
 #' @import dplyr
+
 #' @export
 MetaPredict <- function(userData, taxonList = NULL, cores = 1, output_dir = NULL) {
   if (!is.null(taxonList)) {
@@ -44,6 +45,7 @@ MetaPredict <- function(userData, taxonList = NULL, cores = 1, output_dir = NULL
   #on.exit(future::plan(mplan), add = TRUE)
   #options(future.globals.maxSize = 3145728000)
 
+  cli::cli_alert_info('Reconstructing metabolic pathways and performing prediction calculations...')
 
   #change the code below this line to make the updated function work properly
   if (!is.null(taxonList)) {
@@ -51,7 +53,8 @@ MetaPredict <- function(userData, taxonList = NULL, cores = 1, output_dir = NULL
       #cli::cli_alert_info('Processing {unique(userData$organism)[.x]} ...')
       pull_data(scan_missing[[.x]]$step, taxa$col[[.x]], scan_missing[[.x]],
                 present_modules[[.x]])
-    })
+    }) %>%
+      purrr::set_names(unique(userData$organism))
 
   } else {
     results <- purrr::map(1:length(scan_missing), ~ { #may want to use furrr::future_map() here for optional parallel processing
@@ -73,29 +76,50 @@ MetaPredict <- function(userData, taxonList = NULL, cores = 1, output_dir = NULL
   if (is.null(taxonList)) {
     results <- results %>%
       bind_rows() %>%
-      arrange(module, step)
+      arrange(module, step) %>%
+      mutate(metagenome_name = unique(userData$metagenome_name))
   }
-  cli::cli_alert_success('Finished KEGG metabolic pathway reconstruction and reaction probability calculations.')
-  cli::cli_alert_info('Writing results and summary file. Output is in a list. change (x) to a number of one or more single genomes to view results for a particular genome')
-  cli::cli_alert_info('Enter View(results[[1]][[1]]) to look at summary information, View(results[[(x)]][[2]] for the full results, and View(results[[(x)]][[3]][[(y)]]) for pathway completion heatmaps')
-  cli::cli_alert_info('Change (y) to view heatmaps; e.g., enter 1 to view the first heatmap. For single genome(s), pathways are sorted by their size from the smallest in the first heatmap to the largest in the final heatmap')
-
   if (is.null(taxonList)) {
     summary_information <- summarize_output(results)
-    return(list(summary_information, results))
+    heatmaps <- create_heatmaps_from(results, userData, metagenome = TRUE)
+    output <- list(list(summary_information, results, heatmaps)) %>%
+      purrr::set_names(unique(userData$metagenome_name))
 
+    if (is.null(output_dir)) {
+      cli::cli_alert_success('Finished KEGG metabolic pathway reconstruction and reaction probability calculations. Output is in a list.')
+      cli::cli_alert_info('Enter View(results[[(x)]][[1]]) to look at summary information, View(results[[(x)]][[2]] for the full results, and print(results[[(x)]][[3]]) for pathway completion heatmaps.')
+      cli::cli_alert_warning('Make sure to change (x) to a number between 1 and x, with x equal to the total amount of input genome annotations.')
+      cli::cli_alert_info("To save results, run the following command: save_results(output_dir = '/path/to/output/directory'). Note: the output directory will be created if it does not exist.")
+      return(output)
+    }
   } else {
     summary_information <- purrr::map(results, summarize_output)
-    heatmaps <- purrr::map(results, create_heatmaps_from)
+    heatmaps <- purrr::map(results, ~ {create_heatmaps_from(.x, userData)})
+    output <- purrr::transpose(list(summary_information, results, heatmaps))
 
-    return(purrr::transpose(list(summary_information, results, heatmaps)))
+    if (is.null(output_dir)) {
+      cli::cli_alert_success('Finished KEGG metabolic pathway reconstruction and reaction probability calculations. Output is in a list.')
+      cli::cli_alert_info('Enter View(results[[(x)]][[1]]) to look at summary information, View(results[[(x)]][[2]] for the full results, and print(results[[(x)]][[3]]) for pathway completion heatmaps.')
+      cli::cli_alert_warning('Make sure to change (x) to a number between 1 and x, with x equal to the total amount of input genome annotations.')
+      cli::cli_alert_info("To save results, run the following command: save_results(output_dir = '/path/to/output/directory/'). Note: the output directory will be created if it does not exist.")
+      return(output)
+    }
+  }
+  if (!is.null(output_dir)) {
+    save_results(output, output_dir)
+    cli::cli_alert_success('Finished KEGG metabolic pathway reconstruction and reaction probability calculations. Output is in directory: {output_dir}')
+    return(output)
   }
 }
 
 
 
 format_input <- function(userData) {
-  input_data <- userData %>%
+  input_data <- userData
+  if ('metagenome_name' %in% colnames(userData)) {
+    input_data <- userData %>% select(-metagenome_name)
+  }
+  input_data <- input_data %>%
     select(-c(Gene, data_type)) %>%
     tibble::column_to_rownames(var = 'organism') %>%
     t() %>%
@@ -141,112 +165,146 @@ pull_present_modules_from <- function(tidy_data, userData) {
       tibble::add_column(organism = userData$organism[.x], .before = 1) %>%
       arrange(step) %>%
       group_by(module) %>%
-      dplyr::filter(!all(is.na(probability))) # filters out pathways for which there is no evidence of their presence in the gene annotations;
-    # this should ultimately be a setting that can be turned on/off...
+      dplyr::filter(!all(is.na(probability))) # filters out pathways for which there is no evidence of their presence in the gene annotations; this should ultimately be a setting that can be turned on/off...
+    #if ('metagenome_name' %in% colnames(userData)) {
+    #  present_modules[[.x]] <- present_modules[[.x]] %>%
+    #    mutate(metagenome_name = unique(userData$metagenome_name))
+    #}
   })
   return(present_modules)
 }
 
 
 
-get_splits_from <- function(heatmap_matrices, n_chunks) {
-  keys <- split(unique(heatmap_matrices$module),
-                sort(1:length(unique(heatmap_matrices$module)) %% n_chunks))
+get_splits_from <- function(heatmaps, n_chunks) {
+  keys <- split(unique(heatmaps$module),
+                ggplot2::cut_number(1:length(unique(heatmaps$module)), n_chunks)) %>%
+    purrr::set_names(1:n_chunks)
   return(keys)
 }
 
 
 
-determine_splits_for <- function(heatmap_matrices) {
-  if (n_groups(heatmap_matrices) <= 77) {
-    splits <- get_splits_from(heatmap_matrices, n_chunks = 1)
+determine_splits_for <- function(heatmaps, int = 51, n_ints = 1:15) {
+  ints = int * n_ints
 
-  } else if (n_groups(heatmap_matrices) > 77 & n_groups(heatmap_matrices) <= 154) {
-    splits <- get_splits_from(heatmap_matrices, n_chunks = 2)
+  if (n_groups(heatmaps) <= ints[1]) {
+    splits <- get_splits_from(heatmaps, n_chunks = 1)
 
-  } else if (n_groups(heatmap_matrices) > 154 & n_groups(heatmap_matrices) <= 231) {
-    splits <- get_splits_from(heatmap_matrices, n_chunks = 3)
+  } else if (n_groups(heatmaps) > ints[1] & n_groups(heatmaps) <= ints[2]) {
+    splits <- get_splits_from(heatmaps, n_chunks = 2)
 
-  } else if (n_groups(heatmap_matrices) > 231 & n_groups(heatmap_matrices) <= 308) {
-    splits <- get_splits_from(heatmap_matrices, n_chunks = 4)
+  } else if (n_groups(heatmaps) > ints[2] & n_groups(heatmaps) <= ints[3]) {
+    splits <- get_splits_from(heatmaps, n_chunks = 3)
 
-  } else if (n_groups(heatmap_matrices) > 308 & n_groups(heatmap_matrices) <= 385) {
-    splits <- get_splits_from(heatmap_matrices, n_chunks = 5)
+  } else if (n_groups(heatmaps) > ints[3] & n_groups(heatmaps) <= ints[4]) {
+    splits <- get_splits_from(heatmaps, n_chunks = 4)
 
-  } else if (n_groups(heatmap_matrices) > 385 & n_groups(heatmap_matrices) <= 462) {
-    splits <- get_splits_from(heatmap_matrices, n_chunks = 6)
+  } else if (n_groups(heatmaps) > ints[4] & n_groups(heatmaps) <= ints[5]) {
+    splits <- get_splits_from(heatmaps, n_chunks = 5)
 
-  } else if (n_groups(heatmap_matrices) > 462 & n_groups(heatmap_matrices) <= 539) {
-    splits <- get_splits_from(heatmap_matrices, n_chunks = 7)
+  } else if (n_groups(heatmaps) > ints[5] & n_groups(heatmaps) <= ints[6]) {
+    splits <- get_splits_from(heatmaps, n_chunks = 6)
 
-  } else if (n_groups(heatmap_matrices) > 539 & n_groups(heatmap_matrices) <= 616) {
-    splits <- get_splits_from(heatmap_matrices, n_chunks = 8)
+  } else if (n_groups(heatmaps) > ints[6] & n_groups(heatmaps) <= ints[7]) {
+    splits <- get_splits_from(heatmaps, n_chunks = 7)
 
-  } else if (n_groups(heatmap_matrices) > 616 & n_groups(heatmap_matrices) <= 693) {
-    splits <- get_splits_from(heatmap_matrices, n_chunks = 9)
+  } else if (n_groups(heatmaps) > ints[7] & n_groups(heatmaps) <= ints[8]) {
+    splits <- get_splits_from(heatmaps, n_chunks = 8)
+
+  } else if (n_groups(heatmaps) > ints[8] & n_groups(heatmaps) <= ints[9]) {
+    splits <- get_splits_from(heatmaps, n_chunks = 9)
+
+  } else if (n_groups(heatmaps) > ints[9] & n_groups(heatmaps) <= ints[10]) {
+    splits <- get_splits_from(heatmaps, n_chunks = 10)
+
+  } else if (n_groups(heatmaps) > ints[10] & n_groups(heatmaps) <= ints[11]) {
+    splits <- get_splits_from(heatmaps, n_chunks = 11)
+
+  } else if (n_groups(heatmaps) > ints[11] & n_groups(heatmaps) <= ints[12]) {
+    splits <- get_splits_from(heatmaps, n_chunks = 12)
+
+  } else if (n_groups(heatmaps) > ints[12] & n_groups(heatmaps) <= ints[13]) {
+    splits <- get_splits_from(heatmaps, n_chunks = 13)
+
+  } else if (n_groups(heatmaps) > ints[13] & n_groups(heatmaps) <= ints[14]) {
+    splits <- get_splits_from(heatmaps, n_chunks = 14)
 
   } else {
-    splits <- get_splits_from(heatmap_matrices, n_chunks = 10)
+    splits <- get_splits_from(heatmaps, n_chunks = 15)
   }
   return(splits)
 }
 
 
 
-create_heatmaps_from <- function(results) {
-  heatmap_matrices <- results %>%
-    mutate(probability = case_when(probability == 'Present' ~ '1',
-                                   TRUE ~ probability),
-           step = paste0(seq_along(module)),
-           module_length = length(module)) %>%
-    arrange(module_length) %>%
-    select(-c(full, module_length))
+create_heatmaps_from <- function(results, userData, metagenome = FALSE) {
+  if (metagenome == FALSE) {
+    heatmaps <- results %>%
+      mutate(probability = case_when(probability == 'Present' ~ '1',
+                                     TRUE ~ probability),
+             probability = as.numeric(probability),
+             step = stringr::str_replace(step, 'M\\d{5}_', ''),
+             module_length = length(module)) %>%
+      arrange(module_length) %>%
+      select(-c(full, module_length))
 
-  splits <- determine_splits_for(heatmap_matrices)
+    title_org <- unique(userData$organism)
 
-  heatmap_matrices <- heatmap_matrices %>%
+  } else {
+    heatmaps <- results %>%
+      filter(probability != 'taxonomy not found; no calculation done') %>%
+      mutate(probability = case_when(probability == 'Present' ~ '1',
+                                     TRUE ~ probability),
+             probability = as.numeric(probability)) %>%
+      group_by(module, name, step) %>%
+      summarize(probability = max(probability), .groups = 'drop') %>%
+      group_by(module) %>%
+      mutate(step = stringr::str_replace(step, 'M\\d{5}_', ''),
+             module_length = length(module)) %>%
+      arrange(module_length) %>%
+      select(-module_length)
+
+    title_org <- unique(userData$metagenome_name)
+  }
+  splits <- determine_splits_for(heatmaps)
+
+  heatmaps <- heatmaps %>%
     ungroup() %>%
     mutate(split = purrr::map(1:length(splits), ~ case_when(module %in% splits[[.x]] ~ .x)) %>%
              purrr::as_vector() %>%
              na.omit()) %>%
-    group_by(split) %>%
-    group_split() %>%
-    purrr::map(~ {
-      .x <- .x %>%
-        ungroup() %>%
-        select(name, step, probability) %>%
-        tidyr::pivot_wider(id_cols = step, names_from = name, values_from = probability,
-                    values_fn = toString) %>%
-        select(-step) %>%
-        mutate(across(everything(), as.numeric)) %>%
-        as.matrix() %>%
-        t()
-    })
+    mutate(step = factor(step, levels = unique(step)), name = factor(name, levels = unique(name))) %>%
+    ggplot2::ggplot(ggplot2::aes(step, forcats::fct_rev(name), fill = probability)) +
+    ggplot2::labs(x = 'Module step', y = 'Module name', fill = 'Pathway completion probability',
+                  title = paste0(title_org, ': Metabolic pathway predictions')) +
+    ggplot2::geom_tile(color = 'snow') +
+    ggplot2::theme_bw() +
+    ggplot2::scale_fill_gradient2(low = 'lightskyblue', mid = '#FFFF99', high = 'lightcoral', midpoint = 0.5) +
+    ggplot2::theme(axis.text.y = ggplot2::element_text(size = 4), axis.text.x = ggplot2::element_blank(),
+          axis.ticks.x = ggplot2::element_blank()) +
+    ggplot2::facet_wrap(~ split, scales = 'free')
 
-  heatmap_list <- purrr::map(heatmap_matrices, ~ {
-    pheatmap::pheatmap(.x, cluster_rows = FALSE, cluster_cols = FALSE, show_rownames = TRUE,
-                       cellwidth = 10, cellheight = 9, color = colorRampPalette(c('snow', 'seagreen4'))(100))
-  })
-  return(heatmap_list)
+  return(heatmaps)
 }
 
 
 
-#  if (!is.null(output_dir) & is.null(taxonList)) {
-#    purrr::map(list(summary_information, results), ~ {
-#      readr::write_tsv(.x, file = )
-#    })
-#  }
+#' @export
+save_results <- function(output, output_dir) {
+  if (!dir.exists(output_dir)) {
+    cli::cli_alert_warning('Creating output directory {output_dir}')
+    dir.create(output_dir)
+  }
+  if (!stringr::str_detect(output_dir, '.*/$')) {
+    output_dir <- sub('(.*)', '\\1\\/', output_dir, perl = TRUE)
+  }
+  purrr::map(1:length(output), ~ {
+    readr::write_tsv(output[[.x]][[1]], file = paste0(output_dir, names(output)[.x], '_summary-information.tsv'))
+    readr::write_tsv(output[[.x]][[2]], file = paste0(output_dir, names(output)[.x], '_results.tsv'))
 
-#  if (!is.null(output_dir) & !is.null(taxonList)) {
-#    purrr::map(purrr::transpose(list(summary_information, results)), ~ {
-#      readr::write_tsv(.x[[1]][[1]], file = )
-#    })
-#  }
-#}
-
-#purrr::map2(1:length(results[[1]]), c('summary', 'results'), ~ {
-#  readr::write_tsv(results[[1]][[.x]], paste0('~/Downloads/metapredict-', .y, '.tsv'))
-#})
-
-
+    png(filename = paste0(output_dir, names(output)[.x], '-heatmaps.png'), units = 'in', width = 30, height = 10, res = 500)
+    print(output[[.x]][[3]])
+    dev.off()
+  })
+}
