adeb1027ee35190a5d2388773f02be2a66f1942f
d-mcgrath <mcgrath1@mit.edu>
2021-09-25 22:22:05 -0400

    added anoter eval fn
    Former-commit-id: fbca8bb1d655ffd1a6f562c355afed2b956cc5f8 [formerly fbca8bb1d655ffd1a6f562c355afed2b956cc5f8 [formerly 2a351bfa50e2afa563168a90474e33701566bc1e]]
    Former-commit-id: 3674596d251cad76c424a54190c30fc7aef39c4c
    Former-commit-id: af6ded70ed298df8f2bcb6ee2eb6a7ef137b8bce

--- MetaPredict/R/Evaluate_Performance.R
+++ MetaPredict/R/Evaluate_Performance.R
@@ -104,18 +104,6 @@ evaluate_model_testdata <- function(responseVars, ko_tibble, moduleVector = NULL
     moduleVector = names(responseVars)
   }
 
-  #responseVars <- detect_modules(.data, moduleVector)
-
-  # create list of simulated increments
-  #ko_tibble <- purrr::map_dfr(.data, ~ {
-  #  .x %>%
-  #    dtplyr::lazy_dt() %>%
-  #    dplyr::group_by(genome_name) %>%
-  #    dplyr::summarize(k_numbers = paste0(k_number, collapse = ' ')) %>%
-  #    dplyr::as_tibble()
-  #}) %>%
-  #  dplyr::mutate(k_numbers = stringr::str_split(k_numbers, pattern = ' '))
-
   ko_tibble <- purrr::map(seq(0.10, 1, by = 0.10), function(.prop) {
     ko_tibble %>%
       dplyr::mutate(k_numbers = purrr::map(k_numbers, ~ sample(x = .x)), # randomly shuffle the k_numbers
@@ -188,7 +176,11 @@ evaluate_model_testdata <- function(responseVars, ko_tibble, moduleVector = NULL
 
 
 #' @export
-evaluate_model_testdata.crate <- carrier::crate(function(responseVars, ko_tibble, filler = filler, all_models = all_models) {
+evaluate_model_testdata_dt <- function(responseVars, ko_tibble, moduleVector = NULL) {
+
+  if (is.null(moduleVector)) {
+    moduleVector = names(responseVars)
+  }
 
   ko_tibble <- purrr::map(seq(0.10, 1, by = 0.10), function(.prop) {
     ko_tibble %>%
@@ -197,6 +189,7 @@ evaluate_model_testdata.crate <- carrier::crate(function(responseVars, ko_tibble
       ) %>%
       tidyr::unnest(cols = k_numbers) %>%
       dplyr::mutate(k_count = 1) %>%
+      dtplyr::lazy_dt() %>%
       dplyr::group_by(genome_name, k_numbers) %>%
       dplyr::summarize(k_count = sum(k_count)) %>%
       tidyr::pivot_wider(names_from = k_numbers, values_from = k_count) %>%
@@ -204,6 +197,7 @@ evaluate_model_testdata.crate <- carrier::crate(function(responseVars, ko_tibble
       dplyr::mutate(dplyr::across(2:dplyr::last_col(), ~ as.integer(.x))) %>%
       dplyr::mutate(dplyr::across(2:dplyr::last_col(), ~ dplyr::case_when(is.na(.x) ~ 0L,
                                                                           TRUE ~ .x))) %>%
+      dplyr::as_tibble() %>%
       dplyr::bind_cols(dplyr::select(filler, -c(colnames(filler)[colnames(filler) %in% colnames(.)]))) %>%
       dplyr::select(colnames(filler)) %>%
       dplyr::relocate(colnames(filler)) %>%
@@ -214,14 +208,14 @@ evaluate_model_testdata.crate <- carrier::crate(function(responseVars, ko_tibble
   #for the complete data and each set of simulated incomplete data: predict presence/absence of each KEGG module
   predictions <- purrr::map(ko_tibble, function(.sim_data) {
 
-    purrr::map(all_models[names(responseVars)], ~
+    purrr::map(all_models[moduleVector], ~
                  predict(.x$glmnet.fit,
                          s = .x$lambda.1se,
                          newx = .sim_data,
                          type = 'class')) %>%
       purrr::map_dfc(~ .x) %>%
       dplyr::mutate(dplyr::across(dplyr::everything(), ~ as.double(.x))) %>%
-      dplyr::rename_with(~ names(responseVars))
+      dplyr::rename_with(~ moduleVector)
   })
 
   confusion_matrices <- purrr::map(predictions, function(.iter) {
@@ -257,8 +251,5 @@ evaluate_model_testdata.crate <- carrier::crate(function(responseVars, ko_tibble
   })
 
   return(list(performance_metrics = performance_metrics, confusion_tibble = confusion_tibble))
-})
-
-
-
+}
 
