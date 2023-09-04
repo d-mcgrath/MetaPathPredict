2ee5fc9c956eb183531691391904c2fc7742bb54
David Geller-McGrath <38142818+d-mcgrath@users.noreply.github.com>
2023-09-04 14:18:49 -0400

    Update utils.py
    Former-commit-id: bb563a7110d7f85a62eea905bf5aecf341f5720e [formerly bb563a7110d7f85a62eea905bf5aecf341f5720e [formerly 933c185c499c1781aaa6b2551cd9b530177eab51]]
    Former-commit-id: 3efc5dedad1f3e00430351176786e25576966570
    Former-commit-id: fdc6e38ba856496e47e77509c8b13314bc0aaf2f

--- package/src/metapathpredict/utils.py
+++ package/src/metapathpredict/utils.py
@@ -3,6 +3,8 @@ import pandas as pd
 import re
 
 
+
+
 def read_kofamscan_detailed_tsv(files):
   """Reads in multiple .tsv files, each with columns: 0: "surpassed_threshold", 
   1: 'gene_name', 2: "k_number", 3: "adaptive_threshold", 4: "score",
@@ -23,36 +25,37 @@ def read_kofamscan_detailed_tsv(files):
   
   for file in files:
     lines = []
-    with open(tsv, "rb") as f:
+    with open(file, "rb") as f:
       for row in f:
         if row.decode().split("\t")[0] == "*":
           lines.append(row.decode().split("\t"))
     
     data = pd.DataFrame(lines)
-    data.rename(columns={0: "surpassed_threshold", 1: 'gene_name',
+    data.rename(columns={0: "surpassed_threshold", 1: 'gene_identifier',
     2: "k_number", 3: "adaptive_threshold", 4: "score",
     5: "evalue", 6: "definition"}, inplace=True)
   
-    data[["adaptive_threshold", "score", "evalue"]] = data[["adaptive_threshold", "score", "evalue"]].apply(pd.to_numeric, axis=1)
-    data = data.groupby("gene_name").apply(lambda group: group.loc[group["score"] == group["score"].max()]).reset_index(level=0, drop=True)
+    data[["adaptive_threshold", "score", "evalue"]] = data[["adaptive_threshold", "score", "evalue"]].apply(pd.to_numeric, axis = 1)
+    data = data.groupby("gene_identifier").apply(lambda group: group.loc[group["score"] == group["score"].max()]).reset_index(level = 0, drop = True)
   
-    data["group_size"] = data.groupby(["gene_name"]).transform("size")
+    data["group_size"] = data.groupby(["gene_identifier"]).transform("size")
   
     if data["group_size"].max() > 1:
-        n_genes = (data[['gene_name', 'group_size']].drop_duplicates()['group_size'] > 1).sum()
+        n_genes = (data[['gene_identifier', 'group_size']].drop_duplicates()['group_size'] > 1).sum()
         print(f"""{n_genes} gene(s) contained multiple annotations that surpassed the adaptive threshold. 
         Picking the annotation with the highest score-to-adaptive_threshold ratio for these genes.""")
     
         data["ratio"] = data["score"] / data["adaptive_threshold"]
-        data = data.groupby("gene_name").apply(lambda group: group.loc[group["ratio"] == group["ratio"].max()]).reset_index(level=0, drop=True)
+        data = data.groupby("gene_identifier").apply(lambda group: group.loc[group["ratio"] == group["ratio"].max()]).reset_index(level = 0, drop = True)
   
-        data = data.drop(["ratio"], axis=1)
+        data = data.drop(["ratio"], axis = 1)
   
-    data = data[["gene_identifier", "k_number", "definition"]]
+    data["file_name"] = file
+    data = data[["file_name", "gene_identifier", "k_number", "definition"]]
     
     kofam_list.append(data)
     
-  return data
+  return kofam_list
 
 
 
@@ -82,10 +85,14 @@ def read_dram_annotation_tsv(files):
     data = pd.DataFrame(lines)[[0,8,9]]
     data.rename(columns={0: "gene_identifer", 8: 'k_number',
     9: "definition"}, inplace=True)
+    data["file_name"] = file
+    data = data[["file_name", "gene_identifier", "k_number", "definition"]]
+
     
-    kofam_list.append(data)
+    dram_list.append(data)
     
   return dram_list
+
 
 
 
@@ -102,7 +109,7 @@ def read_koala_tsv(files):
     A list of lists, where each inner list is the annotation data from one file.
   """
   pattern = "K[0-9]{5}"
-  dram_list = []
+  koala_list = []
   
   for file in files:
     lines = []
@@ -114,36 +121,107 @@ def read_koala_tsv(files):
     data = pd.DataFrame(lines)[[0,1,2]]
     data.rename(columns={0: "gene_identifer", 1: 'k_number',
     2: "definition"}, inplace=True)
+    data["file_name"] = file
+    data = data[["file_name", "gene_identifier", "k_number", "definition"]]
+
     
-    kofam_list.append(data)
+    koala_list.append(data)
     
-  return dram_list
+  return koala_list
 
 
 
 
 
-
-
-
-def create_feature_matrix(annotation_list):
+def create_feature_df(annotation_list):
   """Converts as list of annotations into a Pandas feature DataFrame.
 
   Args:
-    x: A list of DataFrames.
+    annotation_list: A list of DataFrames.
 
   Returns:
     A Pandas DataFrame.
   """
   data = pd.DataFrame()
-  for df in x:
+  for df in annotation_list:
     df["count"] = 1
-    data = data.append(df)
-
-  data = data.groupby(["file_name", "k_number"]).agg(count=("count", "sum")).unstack()
-  data.fillna(0, inplace=True)
+    data = pd.concat([data, df], axis = 0)
+  
+  data = data.groupby(["file_name", "k_number"]).agg(count=("count", "sum")).reset_index().pivot_table(
+  index = "file_name",
+  columns = "k_number",
+  values = "count",
+  aggfunc = "first")
+  
+  data = data.replace(np.NaN, 0)
+  data = data.where(data <= 1, 1)
 
   return data
+
+
+
+
+
+def check_feature_columns(required_columns, feature_df)
+  """Checks that all required columns are present for both of MetaPathPredict's models.
+
+  Args:
+    required_columns: A list of all required column names.
+    feature_df: A DataFrame containing all predictor columns for both of MetaPathPredict's models.
+
+  Returns:
+    A Pandas DataFrame.
+  """
+
+  for column in required_columns
+    if column not in feature_df.columns
+      feature_df[column] = 0
+  
+  for column in feature_df.columns
+    if column not in required_columns
+      feature_df.drop([column], axis = 1)
+
+  return feature_df
+
+
+
+
+
+def select_model_features(required_columns, feature_df)
+  """Selects all required columns for the specified MetaPathPredict model (either model #1 or model #2).
+
+  Args:
+    required_columns: A list of all required column names.
+    feature_df: A DataFrame containing all predictor columns for both of MetaPathPredict's models.
+
+  Returns:
+    A Pandas DataFrame.
+  """
+
+  feature_df = feature_df[required_columns]
+
+  return feature_df
+
+
+
+
+
+
+## testing the functions
+
+# fs = ["/Users/davidgeller-mcgrath/Downloads/MAG_172-ko2.tsv", 
+# "/Users/davidgeller-mcgrath/Downloads/MAG_130-ko.tsv", 
+# "/Users/davidgeller-mcgrath/Downloads/MAG_172-kofamscan.tsv"]
+# x = read_kofamscan_detailed_tsv(fs)
+
+
+# g = create_feature_matrix(x)
+# print(g. iloc[:,:10])
+# g.to_csv('/Users/davidgeller-mcgrath/Downloads/sample.tsv', sep="\t", index = False)
+
+
+
+
 
 
 
