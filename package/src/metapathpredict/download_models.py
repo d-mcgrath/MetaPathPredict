2bb70ce5349183fe186675a3faffd015ddf39d65
d-mcgrath <mcgrath1@mit.edu>
2024-10-24 13:54:02 -0400

    updated download_models.py - switched model hub to HuggingFace

--- package/src/metapathpredict/download_models.py
+++ package/src/metapathpredict/download_models.py
@@ -1,6 +1,7 @@
-import pyxet
+#import pyxet
 import importlib
 from importlib import resources
+from huggingface_hub import hf_hub_download
 
 
 class Download:
@@ -17,12 +18,15 @@ class Download:
       print("Downloading MetaPathPredict models...")
       module_dir = resources.files('metapathpredict')
       data_dir = module_dir.joinpath("data/")
-      model_0_dl_path = "xet://dgellermcgrath/MetaPathPredict/main/package/src/metapathpredict/data/model_0.keras"
-      model_1_dl_path = "xet://dgellermcgrath/MetaPathPredict/main/package/src/metapathpredict/data/model_1.keras"
+      # model_0_dl_path = "xet://dgellermcgrath/MetaPathPredict/main/package/src/metapathpredict/data/model_0.keras"
+      # model_1_dl_path = "xet://dgellermcgrath/MetaPathPredict/main/package/src/metapathpredict/data/model_1.keras"
       model_0_install_path = module_dir.joinpath("data/model_0.keras")
       model_1_install_path = module_dir.joinpath("data/model_1.keras")
 
-      fs = pyxet.XetFS()  # fsspec filesystem
-      fs.get(model_0_dl_path, str(model_0_install_path))
-      fs.get(model_1_dl_path, str(model_1_install_path))
+      hf_hub_download(repo_id="dgellermcgrath/MetaPathPredict", filename="model_0.keras", local_dir=model_0_install_path)
+      hf_hub_download(repo_id="dgellermcgrath/MetaPathPredict", filename="model_1.keras", local_dir=model_1_install_path)
+
+      # fs = pyxet.XetFS()  # fsspec filesystem
+      # fs.get(model_0_dl_path, str(model_0_install_path))
+      # fs.get(model_1_dl_path, str(model_1_install_path))
       print("All done. Use MetaPathPredict -h to see how to make predictions.")
