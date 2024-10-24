1a348363276cfe8d38ac3eea0b9ae24619300a7a
d-mcgrath <mcgrath1@mit.edu>
2024-10-24 14:45:41 -0400

    Changed one of the hf_hub_download() arguments

--- package/src/metapathpredict/download_models.py
+++ package/src/metapathpredict/download_models.py
@@ -23,8 +23,8 @@ class Download:
       model_0_install_path = module_dir.joinpath("data/model_0.keras")
       model_1_install_path = module_dir.joinpath("data/model_1.keras")
 
-      hf_hub_download(repo_id="dgellermcgrath/MetaPathPredict", filename="model_0.keras", local_dir=model_0_install_path)
-      hf_hub_download(repo_id="dgellermcgrath/MetaPathPredict", filename="model_1.keras", local_dir=model_1_install_path)
+      hf_hub_download(repo_id="dgellermcgrath/MetaPathPredict", filename="model_0.keras", cache_dir=model_0_install_path)
+      hf_hub_download(repo_id="dgellermcgrath/MetaPathPredict", filename="model_1.keras", cache_dir=model_1_install_path)
 
       # fs = pyxet.XetFS()  # fsspec filesystem
       # fs.get(model_0_dl_path, str(model_0_install_path))
