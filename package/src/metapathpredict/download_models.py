433723f8a100dd79c49977be7861b7189db443cd
d-mcgrath <mcgrath1@mit.edu>
2024-10-24 15:25:07 -0400

    added some code to the download_models.py to extract models from the folders they come in by default from HuggingFace

--- package/src/metapathpredict/download_models.py
+++ package/src/metapathpredict/download_models.py
@@ -1,5 +1,6 @@
 #import pyxet
 import importlib
+import shutil
 from importlib import resources
 from huggingface_hub import hf_hub_download
 
@@ -22,9 +23,24 @@ class Download:
       # model_1_dl_path = "xet://dgellermcgrath/MetaPathPredict/main/package/src/metapathpredict/data/model_1.keras"
       model_0_install_path = module_dir.joinpath("data/model_0.keras")
       model_1_install_path = module_dir.joinpath("data/model_1.keras")
+      
+      model_0_renamed_dir_path = module_dir.joinpath("data/model_0.keras_directory")
+      model_1_renamed_dir_path = module_dir.joinpath("data/model_1.keras_directory")
+      
+      model_0_initial_path = module_dir.joinpath("data/model_0.keras_directory/model_0.keras")
+      model_1_initial_path = module_dir.joinpath("data/model_1.keras_directory/model_1.keras")
 
       hf_hub_download(repo_id="dgellermcgrath/MetaPathPredict", filename="model_0.keras", cache_dir=model_0_install_path)
       hf_hub_download(repo_id="dgellermcgrath/MetaPathPredict", filename="model_1.keras", cache_dir=model_1_install_path)
+
+      shutil.move(model_0_install_path, model_0_renamed_dir_path)
+      shutil.move(model_1_install_path, model_1_renamed_dir_path)
+      
+      shutil.move(model_0_initial_path, model_0_install_path)
+      shutil.move(model_1_initial_path, model_1_install_path)
+
+      shutil.rmtree(model_0_renamed_dir_path)
+      shutil.rmtree(model_1_renamed_dir_path)
 
       # fs = pyxet.XetFS()  # fsspec filesystem
       # fs.get(model_0_dl_path, str(model_0_install_path))
